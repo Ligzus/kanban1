73091c7ef483c8f80fff6f7b688eb2891da4cc26
Алексей Поплаухин <infopop@mail.ru>
2024-05-08 10:58:58 +0300

    Безупешно пытаюсь починить добавление новой карточки

--- skypro-kanban/src/pages/HomePage/HomePage.jsx
+++ skypro-kanban/src/pages/HomePage/HomePage.jsx
@@ -5,22 +5,10 @@ import { cardList, statusList } from "../../data.js";
 import { Loader } from '../../lib/Loader.styled.js'
 import { Outlet } from 'react-router-dom'
 
-function Home() {
+function HomePage() {
 
     const [isLoading, setIsLoading] = useState(true)
     const [cards, setCards] = useState(cardList);
-
-    // const addCard = () => {
-    //     const newCard = {
-    //         id: cards.length + 1,
-    //         category: cardList[cardList.length - 1].category || "Без кактегории", 
-    //         title: "Самая новая задача", 
-    //         date: new Date().toLocaleDateString(), 
-    //         status: statusList[0],
-    //     }
-
-    //     setCards([...cards, newCard]);
-    // }
 
     useEffect(() => {
         setTimeout(() => setIsLoading(false), 500)
@@ -36,11 +24,11 @@ function Home() {
             {/* Компонент Main с прелоадером */}
             {isLoading ? <Loader>Загружаю задачи ...</Loader> : <Main cards={cards} />}  
 
-            <Outlet cards={cards} />
+            <Outlet setCards={setCards} cards={cards} />
             
         </div>
 
     )
 };
 
-export default Home;
+export default HomePage;
--- skypro-kanban/src/pages/NewCardPage/NewCard.jsx
+++ skypro-kanban/src/pages/NewCardPage/NewCard.jsx
@@ -1,13 +1,13 @@
 import PopNewCard from "../../components/PopUps/NewCard/PopNewCard";
-import { cardList } from "../../data";
 import { statusList } from "../../data";
 
-function NewCard() {
+function NewCard({ cards, setCards }) {
 
-    function addCard({ ccc }) {
+    function addCard() {
+        
         const newCard = {
             id: cards.length + 1,
-            category: cardList[cardList.length - 1].category || "Без кактегории", 
+            category: cards[cards.length - 1].category || "Без кактегории", 
             title: "Самая новая задача", 
             date: new Date().toLocaleDateString(), 
             status: statusList[0],
